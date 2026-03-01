716626d1381f41bf686960cfb4492b4359c112ad
admiralbolt <aviknecht@gmail.com>
2026-03-01 13:30:57 -0800

    Fixed up attack animation, took an eternity.

--- michael-aarpg/player.gd
+++ michael-aarpg/player.gd
@@ -4,6 +4,7 @@ var direction: Vector2 = Vector2.ZERO
 var move_speed: float = 100.0
 
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var attack_effect: Sprite2D = %AttackEffectSprite
 @onready var animation_tree: AnimationTree = $AnimationTree
 
 func _ready() -> void:
@@ -11,15 +12,28 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
   direction = Input.get_vector("left", "right", "up", "down")
+  var is_attacking: bool = Input.is_action_pressed("attack")
 
-  if direction != Vector2.ZERO:
-    update_blend_position()
+  animation_tree.set("parameters/conditions/attack", is_attacking)
+  
+  update_blend_position()
+
+func _is_attacking() -> bool:
+  return animation_tree.get("parameters/playback").get_current_node() == "attack"
 
 func _physics_process(_delta: float) -> void:
   velocity = direction * move_speed
+  if _is_attacking():
+    velocity *= 0.66
   move_and_slide()
 
 func update_blend_position() -> void:
+  if direction == Vector2.ZERO or _is_attacking():
+    return
+
   animation_tree.set("parameters/idle/blend_position", direction)
   animation_tree.set("parameters/walk/blend_position", direction)
+  animation_tree.set("parameters/attack/attack/blend_position", direction)
+  animation_tree.set("parameters/attack/attack_effect/blend_position", direction)
   sprite.scale.x = -1 if direction.x < 0 else 1
+  attack_effect.scale.x = -1 if direction.x < 0 else 1
